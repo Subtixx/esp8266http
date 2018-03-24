9824f3faa16d21caa4a0c4b0350e7f7ef6ecf93f
Subtixx <subtixx@gmail.com>
2018-03-24 07:14:55 +0100

    Removed old unused code

--- src/ESP8266Http.cpp
+++ src/ESP8266Http.cpp
@@ -1,154 +1,30 @@
+/*
+	Copyright (C) 2018 Dominic Julien Hock
+		
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "ESP8266Http.h"
 
 HttpResponse Esp8266Http::Post(HttpRequest request)
 {
 	return Request(request);
-	/*
-	WiFiClient client;
-	if (!client.connect(request.Host.c_str(), request.Port))
-	{
-		Serial.println("[Esp8266Http]: Host unreachable");
-		return HttpResponse();
-	}
-	client.print("POST " + request.Path + " HTTP/1.1\r\n");
-	client.print("Host: " + request.Host + "\r\n");
-	for (std::pair<const String, String> headerPair : request.Headers)
-	{
-		client.print(headerPair.first + ": " + headerPair.second + "\r\n");
-	}
-
-	client.print("\r\n");
-
-	client.print(request.QueryString);
-
-	unsigned long requestSend = millis();
-	while (client.available() == 0) {
-		if (millis() - requestSend > request.Timeout) {
-			Serial.println("[Esp8266Http]: Connection to host timed out");
-			client.stop();
-			return HttpResponse();
-		}
-	}
-
-	HttpResponse httpResponse;
-
-	String responseStatusCode = client.readStringUntil('\r');
-	httpResponse.StatusCode = ParseResponse(responseStatusCode);
-
-#ifdef _DEBUG
-	Serial.println("[Esp8266Http]: Response code: " + httpResponse.StatusCode);
-#endif
-
-	while (client.available()) {
-		String headerLine = client.readStringUntil('\r');
-#ifdef _DEBUG
-		Serial.println("[Esp8266Http]: Parsing header: " + headerLine);
-#endif
-
-		size_t pos = headerLine.indexOf(": ");
-		if (pos == -1)
-			break;
-#ifdef _DEBUG
-		Serial.println("[Esp8266Http]: Header: " + headerLine.substring(0, pos) + " - Value: " + headerLine.substring(pos + 2));
-#endif
-		httpResponse.Headers.insert(std::pair<String, String>(headerLine.substring(0, pos), headerLine.substring(pos + 2)));
-	}
-	httpResponse.Body = client.readStringUntil('\r');
-#ifdef _DEBUG
-	Serial.println("[Esp8266Http]: Body: " + httpResponse.Body);
-#endif
-	client.stop();
-	return httpResponse;
-	*/
 }
 
 HttpResponse Esp8266Http::Get(HttpRequest request)
 {
 	return Request(request);
-
-	#ifdef ARDUINO_ARCH_ESP8266
-	/*WiFiClient client;
-	if(!client.connect(request.Host.c_str(), request.Port))
-	{
-		Serial.println("[Esp8266Http]: Host unreachable");
-		return HttpResponse();
-	}
-	client.print(String("GET ") + request.Path + " HTTP/1.1\r\n");
-	client.print("Host: " + request.Host + "\r\n");
-	for (std::pair<const String, String> headerPair : request.Headers)
-	{
-		client.print(headerPair.first + ": " + headerPair.second + "\r\n");
-	}
-
-	client.print("Connection: close\r\n\r\n");
-
-	unsigned long requestSend = millis();
-	while (client.available() == 0) {
-		if (millis() - requestSend > request.Timeout) {
-			Serial.println("[Esp8266Http]: Connection to host timed out");
-			client.stop();
-			return HttpResponse();
-		}
-	}
-
-	HttpResponse httpResponse;
-
-	String responseStatusCode = client.readStringUntil('\r');
-	httpResponse.StatusCode = ParseResponse(responseStatusCode);
-
-#ifdef _DEBUG
-	Serial.println("[Esp8266Http]: Response code: " + httpResponse.StatusCode);
-#endif
-
-	while (client.available()) {
-		String headerLine = client.readStringUntil('\r');
-#ifdef _DEBUG
-		Serial.println("[Esp8266Http]: Parsing header: " + headerLine);
-#endif
-
-		size_t pos = headerLine.indexOf(": ");
-		if (pos == -1)
-			break;
-#ifdef _DEBUG
-			Serial.println("[Esp8266Http]: Header: " + headerLine.substring(0, pos) + " - Value: " + headerLine.substring(pos + 2));
-#endif
-			httpResponse.Headers.insert(std::pair<String, String>(headerLine.substring(0, pos), headerLine.substring(pos + 2)));
-	}
-	httpResponse.Body = client.readStringUntil('\r');
-#ifdef _DEBUG
-	Serial.println("[Esp8266Http]: Body: " + httpResponse.Body);
-#endif
-	client.stop();
-	return httpResponse;
-	*/
-	#endif
-
-	/*
-	WiFi connected
-	IP address: 
-	192.168.178.41
-	[Esp8266Http]: Response code: 200
-	[Esp8266Http]: Parsing header: 
-	Host: 192.168.178.26
-	[Esp8266Http]: Header: 
-	Host - Value: 192.168.178.26
-	[Esp8266Http]: Parsing header: 
-	Connection: close
-	[Esp8266Http]: Header: 
-	Connection - Value: close
-	[Esp8266Http]: Parsing header: 
-	X-Powered-By: PHP/5.6.18
-	[Esp8266Http]: Header: 
-	X-Powered-By - Value: PHP/5.6.18
-	[Esp8266Http]: Parsing header: 
-	Content-type: text/html; charset=UTF-8
-	[Esp8266Http]: Header: 
-	Content-type - Value: text/html; charset=UTF-8
-	[Esp8266Http]: Parsing header: 
-
-	[Esp8266Http]: Body: 
-	1.1
-	 */
 }
 
 HttpResponse Esp8266Http::Request(HttpRequest request)
